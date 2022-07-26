3ca72c594d971a0a26d11b2b8dc29522695a42fb
Sky "g3ner1c" H <g3ner1c-sky@pm.me>
2022-07-26 13:53:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cubing-tools
+
+A bunch of speedcubing related tools, project for learning Svelte
---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+    <title>Cubing tools</title>
+    <meta name="description" content="Cubing tools">
+</svelte:head>
+
+<h1>cubing-tools</h1>
+<p>hi</p>
